b4d8d066a4f7cb35fd83a7aa6541f8670235544d
Shashank1-6 <shashank16reddy09@gmail.com>
2026-04-25 15:20:52 +0530

    removed tracked .history folders

--- Backend/.history/src/routes/route_20260414105945.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const routeController = require("../controllers/routeController");
-
-// GET /route?source=&destination=&type=
-router.get("/route", routeController.getRoute);
-
-module.exports = router;
